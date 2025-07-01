a6d3f3681183f9c516902e0d8fc1e559d3fb9297
Valentin Messias <15677288+messiasv@users.noreply.github.com>
2025-07-02 04:01:20 +0900

    :sparkles: add github oauth (#55)

--- pages/index.vue
+++ pages/index.vue
@@ -8,9 +8,7 @@
         <p class="py-6">
           Keep track of your travels and adventures. Add locations, photos, and notes to create a digital journey of your journeys.
         </p>
-        <button class="btn btn-accent">
-          Sign In With GitHub <Icon name="tabler:brand-github" size="24" />
-        </button>
+        <AuthButton />
       </div>
     </div>
   </div>
