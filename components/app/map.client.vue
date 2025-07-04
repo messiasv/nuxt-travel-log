b95d78d779b915a6ce28f618b3c3721da6ea4e61
Valentin Messias <15677288+messiasv@users.noreply.github.com>
2025-07-05 05:01:18 +0900

    :sparkles: add map to dashboard (#66)

---
+++ components/app/map.client.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { CENTER_JP } from "~/lib/constants";
+
+const colorMode = useColorMode();
+const style = computed(() =>
+  colorMode.value === "dark"
+    ? "/styles/dark.json"
+    : "https://tiles.openfreemap.org/styles/liberty");
+const zoom = 6;
+</script>
+
+<template>
+  <MglMap
+    :map-style="style"
+    :center="CENTER_JP"
+    :zoom="zoom"
+  >
+    <MglNavigationControl />
+  </MglMap>
+</template>
