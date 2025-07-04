daae53d96a607dfaf233a89cc38f380126e1ca3e
Valentin Messias <15677288+messiasv@users.noreply.github.com>
2025-07-05 03:32:39 +0900

    :sparkles: add locations to sidebar

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
 const isSidebarOpen = ref(true);
+const route = useRoute();
+const sidebarStore = useSidebarStore();
+const locationStore = useLocationStore();
 
 onMounted(() => {
   isSidebarOpen.value = localStorage.getItem("is-sidebar-open") === "true";
+  if (route.path !== "/dashboard") {
+    locationStore.refresh();
+  }
 });
 
 function toggleSidebar() {
@@ -31,6 +37,20 @@ function toggleSidebar() {
           icon="tabler:circle-plus-filled"
           href="/dashboard/add"
         />
+        <div v-if="sidebarStore.loading || sidebarStore.sidebarItems.length > 0" class="divider" />
+        <div v-if="sidebarStore.loading" class="px-4">
+          <div class="skeleton h-4 w-full" />
+        </div>
+        <div v-else-if="sidebarStore.sidebarItems.length > 0" class="flex flex-col">
+          <SidebarButton
+            v-for="item in sidebarStore.sidebarItems"
+            :key="item.id"
+            :show-label="isSidebarOpen"
+            :label="item.label"
+            :icon="item.icon"
+            :href="item.href"
+          />
+        </div>
         <div class="divider" />
         <SidebarButton
           :show-label="isSidebarOpen"
