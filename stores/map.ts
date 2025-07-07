233b197afc8fce2296527863dc43d2d3ab37d74b
Valentin Messias <15677288+messiasv@users.noreply.github.com>
2025-07-08 01:47:21 +0900

    :sparkles: Add map popups and selected point (#68)

--- stores/map.ts
+++ stores/map.ts
@@ -1,7 +1,16 @@
+import type { LngLatBounds } from "maplibre-gl";
+
 import type { MapPoint } from "~/lib/types";
 
 export const useMapStore = defineStore("mapStore", () => {
   const mapPoints = ref<MapPoint[]>([]);
+  const selectedPoint = ref<MapPoint | null>(null);
+  const shouldFlyTo = ref(true);
+
+  function selectPointWithoutFlyTo(point: MapPoint | null) {
+    shouldFlyTo.value = false;
+    selectedPoint.value = point;
+  }
 
   async function init() {
     const { useMap } = await import("@indoorequal/vue-maplibre-gl");
@@ -9,13 +18,16 @@ export const useMapStore = defineStore("mapStore", () => {
 
     const map = useMap();
 
+    let bounds: LngLatBounds | null = null;
+    const padding = 60;
+
     effect(() => {
       const firstPoint = mapPoints.value[0];
       if (!firstPoint) {
         return;
       }
 
-      const bounds = mapPoints.value.reduce((bounds, point) => {
+      bounds = mapPoints.value.reduce((bounds, point) => {
         return bounds.extend([point.long, point.lat]);
       }, new LngLatBounds(
         [firstPoint.long, firstPoint.lat],
@@ -23,13 +35,32 @@ export const useMapStore = defineStore("mapStore", () => {
       ));
 
       map.map?.fitBounds(bounds, {
-        padding: 60,
+        padding,
       });
+    });
+
+    effect(() => {
+      if (selectedPoint.value) {
+        if (shouldFlyTo.value) {
+          map.map?.flyTo({
+            center: [selectedPoint.value.long, selectedPoint.value.lat],
+            speed: 0.8,
+          });
+        }
+        shouldFlyTo.value = true;
+      }
+      else if (bounds) {
+        map.map?.fitBounds(bounds, {
+          padding,
+        });
+      }
     });
   }
 
   return {
     init,
     mapPoints,
+    selectedPoint,
+    selectPointWithoutFlyTo,
   };
 });
