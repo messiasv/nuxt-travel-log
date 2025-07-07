60d0ed8c47d3246ddb15f57561f5d0824bb6d843
Valentin Messias <15677288+messiasv@users.noreply.github.com>
2025-07-07 22:54:43 +0900

    :sparkles: show points on the map (#67)
    * :sparkles: add map markers
    
    * :sparkles: change linting line wrap
    
    * :sparkles: add custom icon to map marker
    
    * :sparkles: fit map to location points
    
    * :sparkles: Adjust style

--- components/app/map.client.vue
+++ components/app/map.client.vue
@@ -2,11 +2,16 @@
 import { CENTER_JP } from "~/lib/constants";
 
 const colorMode = useColorMode();
+const mapStore = useMapStore();
 const style = computed(() =>
   colorMode.value === "dark"
     ? "/styles/dark.json"
     : "https://tiles.openfreemap.org/styles/liberty");
 const zoom = 6;
+
+onMounted(() => {
+  mapStore.init();
+});
 </script>
 
 <template>
@@ -16,5 +21,20 @@ const zoom = 6;
     :zoom="zoom"
   >
     <MglNavigationControl />
+    <MglMarker
+      v-for="point in mapStore.mapPoints"
+      :key="point.id"
+      :coordinates="[point.long, point.lat]"
+    >
+      <template #marker>
+        <div class="tooltip" :data-tip="point.label">
+          <Icon
+            name="tabler:map-pin-filled"
+            size="30"
+            class="text-secondary"
+          />
+        </div>
+      </template>
+    </MglMarker>
   </MglMap>
 </template>
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,3 @@
-export const CENTER_JP = [139.7419, 35.6931];
+import type { LngLatLike } from "maplibre-gl";
+
+export const CENTER_JP = [139.7419, 35.6931] as LngLatLike;
