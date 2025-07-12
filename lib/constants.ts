529a0c5c6958b805b404d395eee9e53809d401de
Valentin Messias <15677288+messiasv@users.noreply.github.com>
2025-07-13 01:03:46 +0900

    :sparkles: view location log

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,6 +3,11 @@ import type { LngLatLike } from "maplibre-gl";
 export const CENTER_JP = [139.7419, 35.6931] as LngLatLike;
 
 export const LOCATION_PAGES = new Set(["dashboard", "dashboard-add"]);
+export const CURRENT_LOCATION_LOG_PAGES = new Set([
+  "dashboard-location-slug-id",
+  "dashboard-location-slug-id-add",
+  "dashboard-location-slug-id-edit",
+]);
 export const CURRENT_LOCATION_PAGES = new Set([
   "dashboard-location-slug",
   "dashboard-location-slug-add",
