16ae07c86fca3baa67410e142bc32d859b9d41a4
Valentin Messias <15677288+messiasv@users.noreply.github.com>
2025-07-15 20:40:53 +0900

    :sparkles: add lightbox gallery (#97)

--- pages/dashboard/location/[slug]/[id]/images.vue
+++ pages/dashboard/location/[slug]/[id]/images.vue
@@ -183,7 +183,7 @@ async function uploadImage() {
         </button>
       </div>
       <ImageList class="ml-2" :images="locationLog?.images || []">
-        <template #default="{ image: item }">
+        <template #card-bottom="{ image: item }">
           <button
             :disabled="deletingImage === item && isDeleting"
             class="btn btn-xs btn-error"
